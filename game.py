f8d107e4cd08f69636b31249556e6c077a3f96cf
Sicilat <sicilat@gmail.com>
2019-03-23 20:07:11 +0100

    Main menu finished, Base functions added

--- game.py
+++ game.py
@@ -8,6 +8,17 @@ import os, sys
 def clear():
 	os.system('clear||cls')
 
+def setup_game():
+	print('setup')
+
+def show_help_menu():
+	clear()
+	print('This is the help menu !')
+	print('All this current page can be found into the official documentation !')
+
+def load_game():
+	print('WIP')
+
 def handle_main_menu_display():
 	clear()
 	print('###################')
@@ -16,10 +27,25 @@ def handle_main_menu_display():
 	print('###################')
 	print('       -Play-      ')
 	print('       -Load-      ')
-	print('       -Help-     ')
+	print('       -Help-      ')
 	print('       -Quit-      ')
+	handle_main_menu_options()
 
 def handle_main_menu_options():
-
+	asw = input('> ')
+	while asw.lower() not in ['play', 'load', 'help', 'quit', 'start', 'exit', 'about']:
+		print('Please enter a supported answer !')
+		asw = input('> ')
+	if asw.lower() == 'play' or asw.lower() == 'start':
+		setup_game()
+	elif asw.lower() == 'load':
+		load_game()
+	elif asw.lower() == 'help' or asw.lower() == 'about':
+		show_help_menu()
+	else:
+		clear()
+		print('Thank you for playing !')
+		sys.exit()
 
 print("Here's the main program !")
+handle_main_menu_display()
