de1e09b855adb38b16806af7c091b33a6c8cff31
Sicilat <sicilat@gmail.com>
2019-03-23 19:42:24 +0100

    Added Main menu display

--- game.py
+++ game.py
@@ -3,4 +3,23 @@
 # Made by Sicilat #
 ###################
 
+import os, sys
+
+def clear():
+	os.system('clear||cls')
+
+def handle_main_menu_display():
+	clear()
+	print('###################')
+	print('#  TES-Adventure  #')
+	print('# Made by Sicilat #')
+	print('###################')
+	print('       -Play-      ')
+	print('       -Load-      ')
+	print('       -Help-     ')
+	print('       -Quit-      ')
+
+def handle_main_menu_options():
+
+
 print("Here's the main program !")
